148f21d8c5e648abbe141c9ca8eddf17832a2e1a
Jagadeesh Talari <talarijagadeesh55@gmail.com>
2022-12-11 23:20:41 +0530

    Added stop and reset functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let start = document.getElementById("start");
 let stop = document.getElementById("stop");
+let reset = document.getElementById("reset");
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
@@ -8,9 +9,12 @@ let millisecondsCount = 00;
 let secondsCounter = 00;
 let minutesCounter = 00;
 let hoursCounter = 00;
+let nIntervalID;
 
 start.addEventListener( 'click', () => {
-    setInterval( () => {
+   
+if (!nIntervalID){
+    nIntervalID = setInterval( () => {
         
         millisecondsCount = millisecondsCount + 1;
         milliseconds.innerText = millisecondsCount<10? `0${millisecondsCount}`: millisecondsCount;
@@ -29,5 +33,23 @@ start.addEventListener( 'click', () => {
             hoursCounter = hoursCounter + 1;
             hours.innerText = hoursCounter;
         }   
-    }, 10)
+    }, 10);
+
+}
+
 })
+
+stop.addEventListener( 'click', stopfunc);
+
+reset.addEventListener( 'click', () => {
+   stopfunc();
+   milliseconds.innerText = '00';
+   seconds.innerText = '00';
+   minutes.innerText = '00';
+   hours.innerText = '00';
+});
+
+function stopfunc(){
+    clearInterval(nIntervalID);
+    nIntervalID = null;
+}
